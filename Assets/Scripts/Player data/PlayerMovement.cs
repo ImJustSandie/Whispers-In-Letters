ee0fc06bdd1044653e74aea228b127af5c631fb5
WilliamCubilloSR75 <est.williams.cubil1@unimilitar.edu.co>
2026-04-18 18:23:39 -0500

    Correcciones 3D, animator 2D

--- Assets/Scripts/Player data/PlayerMovement.cs
+++ Assets/Scripts/Player data/PlayerMovement.cs
@@ -67,7 +67,7 @@ public class PlayerMovement : MonoBehaviour
         if (Animator != null)
         {
             Animator.SetFloat("MovementA", moveInput.magnitude);
-            Animator.SetBool("Sprint", controls.Player.Sprint.IsPressed());
+            Animator.SetBool("Sprint", controls.Player.Sprint.IsPressed() && moveInput.magnitude > 0.01f);
         }
     }
 }
